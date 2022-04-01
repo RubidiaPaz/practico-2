db54c7c404d78c3c2546bc240a37b5cfe5439246
WILLIAMS AMAYA <whistemalo@gmail.com>
2022-03-31 23:54:19 -0600

    modificacion al registro

--- Practica2/agregar.php
+++ Practica2/agregar.php
@@ -1,10 +1,10 @@
 <?php
     $materias = simplexml_load_file("./materias.xml");
     $materia = $materias->addChild('materia');
-    $materia->addChild('codigo', $_POST['codigo']);
     $materia->addChild('nombre', $_POST['nombre']);
-    $materia->addChild('uvs', $_POST['uvs']);
-    $materia->addChild('nota', $_POST['nota']);
+    $materia->addChild('tarea', $_POST['tarea']);
+    $materia->addChild('investigacion', $_POST['investigacion']);
+    $materia->addChild('examen', $_POST['examen']);
 
     file_put_contents('./materias.xml', $materias->asXML());
     session_start();
--- Practica2/nueva_modal.php
+++ Practica2/nueva_modal.php
@@ -4,14 +4,14 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <center><h4 class="modal-title" id="myModalLabel">Nueva materia</h4></center>
+                <center><h4 class="modal-title" id="myModalLabel">Ingreso de Notas</h4></center>
             </div>
             <div class="modal-body">
 			<div class="container-fluid">
 			<form method="POST" action="agregar.php">
 				<div class="row form-group">
 					<div class="col-sm-2">
-						<label class="control-label" for="codigo">Codigo:</label>
+						<label class="control-label" for="codigo">Nombre:</label>
 					</div>
 					<div class="col-sm-10">
 						<input type="text" class="form-control" name="codigo" id="codigo">
@@ -19,7 +19,7 @@
 				</div>
 				<div class="row form-group">
 					<div class="col-sm-2">
-						<label class="control-label" for="nombre">Nombre:</label>
+						<label class="control-label" for="nombre">Tarea:</label>
 					</div>
 					<div class="col-sm-10">
 						<input type="text" class="form-control" name="nombre" id="nombre">
@@ -27,7 +27,7 @@
 				</div>
 				<div class="row form-group">
 					<div class="col-sm-2">
-						<label class="control-label" for="uvs">UVS:</label>
+						<label class="control-label" for="uvs">Investigacion:</label>
 					</div>
 					<div class="col-sm-10">
 						<input type="number" min="2" max="5" class="form-control" name="uvs" id="uvs">
@@ -35,7 +35,7 @@
 				</div>
 				<div class="row form-group">
 					<div class="col-sm-2">
-						<label class="control-label" for="nota" >Nota:</label>
+						<label class="control-label" for="nota" >Examen Parcial:</label>
 					</div>
 					<div class="col-sm-10">
 						<input type="number" min="0" max="10" step="0.1" class="form-control" name="nota" id="nota">
