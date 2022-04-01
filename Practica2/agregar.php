f2d850602a77ddb0845862404d54272a3cb4c0ed
WILLIAMS AMAYA <whistemalo@gmail.com>
2022-04-01 00:32:04 -0600

    promedio calculado

--- Practica2/agregar.php
+++ Practica2/agregar.php
@@ -5,8 +5,8 @@
     $materia->addChild('tarea', $_POST['tarea']);
     $materia->addChild('investigacion', $_POST['investigacion']);
     $materia->addChild('examen', $_POST['examen']);
-    $promedio= $_POST['tarea'] + $_POST['investigacion'] +$_POST['examen'];
-    $materia->addChild('promedio', ($promedio/3));
+    $promedio= ($_POST['tarea']*0.5) + ($_POST['investigacion']*0.3) +($_POST['examen']*0.20);
+    $materia->addChild('promedio', (round($promedio,2)));
 
     file_put_contents('./materias.xml', $materias->asXML());
     session_start();
