f70d08d98c52af9c4274a48c9d32d2b9a22a2400
WILLIAMS AMAYA <whistemalo@gmail.com>
2022-03-31 23:14:19 -0600

    guia6

--- guia6.php
+++ guia6.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Calculo de Salario</title>
+    <title>Tablas de multiplicar</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0" />
     <meta http-equiv="Content-Script-Type" content="text/javascript" />
@@ -45,78 +45,100 @@
 <section>
 <article>
 <div id="empleado">
-  <div class="formoid-solid-purple">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                  <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                      <li class="nav-item">
-                        <a class="nav-link " aria-current="page" href="index.php">GUIA 5</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link active" href="guia6.php">GUIA 6</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link " href="guia7.php">GUIA 7</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link " href="guia8.php">GUIA 8</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </nav>
-        <div class="title">
-            <h2>Tabla de Multiplicar</h2>
+<form  method="POST" name="numero" id="numero" class="formoid-solid-purple">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="index.php">GUIA 5</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="guia6.php">GUIA 6</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="guia7.php">GUIA 7</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " href="guia8.php">GUIA 8</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="element-name">
-            <label class="title"></label>
-            <div class="nameFirst">
-                <input type="text" name="empleado" id="txtempleado" maxlength="50" placeholder="Nombre empleado" allowed="ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚáéíóúÜü" messageid="alfabeticchars" class="large" />
-                <span class="icon-place"></span>
-            </div>
-            <span id="alfabeticchars">Solo acepta caracteres alfabéticos</span>
-        </div>
-        <div class="element-number">
-            <label class="title"></label>
-            <div class="item-cont">
-                <input type="text" name="sueldobase" id="sueldobase" maxlength="12" placeholder="Sueldo base" allowed="0123456789." messageid="numbersonly" class="large" />
-                <span class="icon-place"></span>
-            </div>
-            <span id="numbersonly">Solo acepta números y punto decimal</span>
-        </div>
-        <div class="element-checkbox">
-            <label class="title">Horas extras:</label>
-            <div class="column column1">
-                <input type="checkbox" name="hextra" id="hextrasi" value="no" />
-                <span>Habilitar</span>
-            </div>
-        </div>
-        <div class="element-number">
-            <label class="title"></label>
-            <div class="item-cont">
-                <input type="text" name="numhorasex" id="numhorasex" maxlength="2" disabled="disabled" placeholder="Número de horas" allowed="0123456789" messageid="integersonly" />
-                <span class="icon-place"></span>
-            </div>
-            <span id="integersonly">Solo acepta números enteros</span>
-        </div>
-        <div class="element-number">
-            <label class="title"></label>
-            <div class="item-cont">
-                <input type="text" name="pagohextra" id="pagohextra" size="12" maxlength="6" disabled="disabled" placeholder="Pago hora extra" allowed="0123456789." messageid="othernumbersonly" />
-                <span class="icon-place"></span>
-            </div>
-            <span id="othernumbersonly">Solo acepta números y punto decimal</span>
-        </div>
-        <div class="submit">
-            <input type="submit" name="enviar" id="enviar" value="Enviar" />
-            <input type="reset" name="reset" id="reset" value="Restablecer" />
-        </div>
-        </form>
-        </div>
-        </article>
-        </section>
+      </nav>
+<div class="title">
+    <h2>Tabla de Multiplicar</h2>
 </div>
+
+
+<!-- numero para la tabla -->
+<div class="element-number">
+    <label class="title"></label>
+    <div class="item-cont">
+        <input type="text" name="numero" id="numero" maxlength="12" placeholder="Numero" allowed="0123456789." messageid="numbersonly" class="large" />
+        <span class="icon-place"></span>
+    </div>
+    <span id="numbersonly">Solo acepta números y punto decimal</span>
+</div>
+
+<!-- multiplo final  -->
+<div class="element-number">
+    <label class="title"></label>
+    <div class="item-cont">
+        <input type="text" name="multiplo" id="multiplo" maxlength="12" placeholder="Multiplo" allowed="0123456789" messageid="numbersonly" class="large" />
+        <span class="icon-place"></span>
+    </div>
+    <span id="numbersonly">Solo acepta números enteros</span>
+</div>
+
+
+<?php
+        
+        if(isset($_POST['enviar']))
+        {
+          $numero = (isset($_POST['numero'])) ? doubleval($_POST['numero']) : 0.0;
+          print("<h3>Tabla de multiplicar de $numero </h3>");
+          Calcular_tabla();
+        
+        }
+?>
+
+<?php
+
+
+       function Calcular_tabla(){
+       $numero = (isset($_POST['numero'])) ? doubleval($_POST['numero']) : 0.0;
+       $multiplo = (isset($_POST['multiplo'])) ? doubleval($_POST['multiplo']) : 0.0;
+
+       echo "<ul>";
+      
+       for($i=1;$i<=$multiplo;$i++){
+         if ($i%2==0){ // Es par $i
+           echo "<li><span style='color:red'>$numero x $i = ";
+           echo $numero*$i;
+           echo "</span></li>";                            
+         }    //Fin del if
+         else{
+           echo "<li>$numero x $i = ";
+           echo $numero*$i;
+           echo "</li>";
+         } //Fin del else
+       } //Fin del for            
+                 
+       echo "</ul>";
+       
+      }
+  
+    ?>
+
+<div class="submit">
+    <input type="submit" name="enviar" id="enviar" value="Enviar" />
+    <input type="reset" name="reset" id="reset" value="Restablecer" />
+</div>
+</form>
+</div>
+</article>
+</section>
 <script src="js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 <script src="js/sidebars.js"></script>
