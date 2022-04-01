4005ebd937173c85a3a6b04e2161ff4e34fc868f
RubidiaPaz <rubigarcia190@gmail.com>
2022-03-31 23:11:02 -0600

    guia7

--- guia7.php
+++ guia7.php
@@ -66,54 +66,63 @@
           </div>
         </div>
       </nav>
-<div class="title">
-    <h2>Expresiones Regulares</h2>
-</div>
-<div class="element-name">
-    <label class="title"></label>
-    <div class="nameFirst">
-        <input type="text" name="empleado" id="txtempleado" maxlength="50" placeholder="Nombre empleado" allowed="ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚáéíóúÜü" messageid="alfabeticchars" class="large" />
-        <span class="icon-place"></span>
+    <div class="title">
+        <h2>Expresiones Regulares</h2>
     </div>
-    <span id="alfabeticchars">Solo acepta caracteres alfabéticos</span>
-</div>
-<div class="element-number">
-    <label class="title"></label>
-    <div class="item-cont">
-        <input type="text" name="sueldobase" id="sueldobase" maxlength="12" placeholder="Sueldo base" allowed="0123456789." messageid="numbersonly" class="large" />
-        <span class="icon-place"></span>
+    <div class="element-name">
+        <label class="title"></label>
+        <div id="bodywrap">
+        <div id="contents"> 
+            <section id="main"> 
+                <div id="leftcontainer"> 
+                    <section id="sidebar"> 
+                        <?php
+                            if(isset($_POST['Enviar']))
+                            {
+                                $text = isset($_POST['comment']) ? trim($_POST['comment']) : null;
+                                $palabra = isset($_POST['palabra']) ? trim($_POST['palabra']) : null;
+                                $text = preg_replace("/\b(".$palabra.")\b/i", '<span style="background:#5fc9f6">\1</span>', $text); 
+                                $number = preg_match_all("/\b(".$palabra.")\b/i",$text,$counter);
+                                echo "<b> Hay $number Coincidencias de $palabra</b>"; 
+                        ?> <br>
+                        <div id="sidebarwrap">
+                            <br><b>Resultado</b>
+                            <br><p><?=$text?></p> 
+                        </div> 
+                        <?php 
+                            }
+                        ?> 
+                    </section>
+
+                    <div class="clear"></div>
+                    <article class="post">
+                        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" class="form">
+                            <p class="textfield"> 
+                                <label for="palabra"> <small>Palabra a buscar</small> </label> 
+                                <input name="palabra" id="palabra" value="tierra" size="22" tabindex="1" type="text" /> 
+                            </p> 
+                            <p> 
+                                <small>Ingrese el texto de prueba para procesarlo con las <strong>expresiones regulares</strong> </small>
+                            </p>
+                            <p class="text-area">
+                                <textarea name="comment" id="comment" cols="50" rows="10" tabindex="4"> A 39 años luz de la tierra se encuentra la estrella Trappist-1, conocida en la NASA por el nombre de catálogo 2MASS J23062928-0502285. Es decir, un astro completamente irrelevante hasta hace muy poco. El pasado mayo, el equipo liderado por Michaël Gillon (Universidad de Lieja, Bélgica) ya anunció que había descubierto dos planetas (quizás tres) orbitándola. Con un 9% de la masa del Sol, Trappist-1 es una enana roja fría. Si fuera un poco más pequeña, ya no sería una estrella sino una enana marrón, que son astros fríos que no pueden fusionar hidrógeno como nuestro sol. Ahora la NASA ha anunciado que Trappist-1 no tiene tres, sino siete exoplanetas, todos ellos de tamaño similar al de la Tierra, tres o cuatro de los cuales están en órbitas templadas donde no hace ni demasiado frío ni demasiado calor para permitir la existencia de agua líquida en su superficie y, por lo tanto, vida tal como la conocemos. Al ser la estrella pequeña y mucho más débil que nuestro sol, los exoplanetas están en órbitas necesariamente muy compactas. Es más, si pusiéramos los planetas en torno a nuestro sol, todos cabrían cómodamente dentro de la órbita de Mercurio. 
+
+                                </textarea> 
+                            </p>
+                            <p> 
+                                <input name="Enviar" id="Enviar" value="1" type="hidden" /> 
+                                <input name="submit" id="submit" tabindex="5" type="image" src="/img/submit.png" />
+                            </p>
+                            <div class="clear"></div> 
+                        </form> 
+                        <div class="clear"></div> 
+                    </article> 
+                </div> 
+            </section> 
+            <div class="clear"></div> 
+        </div> 
+    </div> 
     </div>
-    <span id="numbersonly">Solo acepta números y punto decimal</span>
-</div>
-<div class="element-checkbox">
-    <label class="title">Horas extras:</label>
-    <div class="column column1">
-        <input type="checkbox" name="hextra" id="hextrasi" value="no" />
-        <span>Habilitar</span>
-    </div>
-</div>
-<div class="element-number">
-    <label class="title"></label>
-    <div class="item-cont">
-        <input type="text" name="numhorasex" id="numhorasex" maxlength="2" disabled="disabled" placeholder="Número de horas" allowed="0123456789" messageid="integersonly" />
-        <span class="icon-place"></span>
-    </div>
-    <span id="integersonly">Solo acepta números enteros</span>
-</div>
-<div class="element-number">
-    <label class="title"></label>
-    <div class="item-cont">
-        <input type="text" name="pagohextra" id="pagohextra" size="12" maxlength="6" disabled="disabled" placeholder="Pago hora extra" allowed="0123456789." messageid="othernumbersonly" />
-        <span class="icon-place"></span>
-    </div>
-    <span id="othernumbersonly">Solo acepta números y punto decimal</span>
-</div>
-<div class="submit">
-    <input type="submit" name="enviar" id="enviar" value="Enviar" />
-    <input type="reset" name="reset" id="reset" value="Restablecer" />
-</div>
-</form>
-</div>
 </article>
 </section>
     </div>
